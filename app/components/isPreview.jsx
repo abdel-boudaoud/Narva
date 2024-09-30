df6efcbb03a08b0e418d30493ec6d8510daebc0a
abdel-boudaoud <abdelkaderfarm@gmail.com>
2024-09-30 23:31:48 +0100

    update isPreview

--- app/components/isPreview.jsx
+++ app/components/isPreview.jsx
@@ -1,7 +1,7 @@
 const isServer = typeof window === "undefined";
 const isPreview = () => {
   let isPreviewValue = isServer
-    ? process.env.STORYBLOK_IS_PREVIEW
+    ? import.meta.env.STORYBLOK_IS_PREVIEW
     : window.env.STORYBLOK_IS_PREVIEW;
 
   return isPreviewValue === "yes";
